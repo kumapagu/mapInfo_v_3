852e8642d4e557cc700a73769ac7530b9384eb70
kumapagu <kmno_555@yahoo.co.jp>
2022-01-20 11:50:57 +0900

    settings.pyの変更

--- map_info/map_info/settings.py
+++ map_info/map_info/settings.py
@@ -25,7 +25,7 @@ from .settings_secret import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
