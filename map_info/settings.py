16b14296998776e9d85b910f8f3c95f11b252512
kumapagu <kmno_555@yahoo.co.jp>
2022-02-02 03:13:36 +0900

    デプロイ用

--- map_info/settings.py
+++ map_info/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 
-from map_info.settings_secret import SECRET_KEY
+# from map_info.settings_secret import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .settings_secret import *
-# SECRET_KEY = os.environ['SECRET_KEY']
+# from .settings_secret import *
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
