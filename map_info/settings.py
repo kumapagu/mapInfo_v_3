6528593df94fef85223b66dd62b06fab93afae64
kumapagu <kmno_555@yahoo.co.jp>
2022-02-02 12:05:08 +0900

    デプロイ用

--- map_info/settings.py
+++ map_info/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/dist/static'),
+    os.path.join(BASE_DIR, 'frontend/dist/static/'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
