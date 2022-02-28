1334cd4d442a6d847b28e9494d44490669042982
kumapagu <kmno_555@yahoo.co.jp>
2022-02-28 20:22:49 +0900

    urls.pyの編集

--- map_app/urls.py
+++ map_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # re_path('', views.IndexView.as_view(), name='entry-point'),
     path('', views.IndexView.as_view(), name='entry-point'),
+    path('park/<int:id>', views.IndexView.as_view()),
 ]
